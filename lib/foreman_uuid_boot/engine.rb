269fbb416355c4775f4965a1724431bf8b8e7c4f
Alexander Olofsson <alexander.olofsson@liu.se>
2023-01-20 14:21:15 +0100

    Improve startup messaging

--- lib/foreman_uuid_boot/engine.rb
+++ lib/foreman_uuid_boot/engine.rb
@@ -28,16 +28,20 @@ module ForemanUuidBoot
     config.to_prepare do
       ::Foreman::UnattendedInstallation::HostFinder.prepend ForemanUuidBoot::HostFinderExtensions
 
-      # Slight hack for foreman_discovery - to store UUID for provision
-      begin
-        # Ensure that Host::Discovered applies host facets
-        unless ::Host::Discovered.include? Facets::ModelExtensionsBase
-          ::Host::Discovered.include SelectiveClone
-          ::Host::Discovered.include Facets::ManagedHostExtensions
-          ::Host::Discovered.prepend ForemanUuidBoot::HostDiscoveredExtensions
+      if Foreman::Plugin.installed?('foreman_discovery')
+        # Slight hack for foreman_discovery - to store UUID for provision
+        begin
+          # Ensure that Host::Discovered applies host facets
+          unless ::Host::Discovered.include? Facets::ModelExtensionsBase
+            ::Host::Discovered.include SelectiveClone
+            ::Host::Discovered.include Facets::ManagedHostExtensions
+            ::Host::Discovered.prepend ForemanUuidBoot::HostDiscoveredExtensions
+
+            Rails.logger.info 'ForemanUuidBoot: Extended foreman_discovery with facet support'
+          end
+        rescue StandardError => e
+          Rails.logger.warn "ForemanUuidBoot: Failed to activate discovery extensions (#{e})"
         end
-      rescue NameError => e
-        Rails.logger.warn "ForemanUuidBoot: Discovery not installed(#{e}), skipping discovery extensions"
       end
     rescue StandardError => e
       Rails.logger.warn "ForemanUuidBoot: skipping engine hook(#{e})"

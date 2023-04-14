d9cc141db93320d83b91a4cfb311071022c45ca3
Alexander Olofsson <alexander.olofsson@liu.se>
2023-04-14 13:09:48 +0200

    Populate facet fields before main field population

--- app/models/concerns/foreman_uuid_boot/host_discovered_extensions.rb
+++ app/models/concerns/foreman_uuid_boot/host_discovered_extensions.rb
@@ -3,8 +3,9 @@
 module ForemanUuidBoot
   module HostDiscoveredExtensions
     def populate_fields_from_facts(parser, type, source_proxy)
-      super
       populate_facet_fields(parser, type, source_proxy)
+
+      super(parser, type, source_proxy)
     end
   end
 end

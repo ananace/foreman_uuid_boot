fcf86bc0c3203b236053c027adbd7bcbbd7c592c
Alexander Olofsson <alexander.olofsson@liu.se>
2023-11-27 16:08:56 +0100

    Handle platforms without UUIDs

--- app/models/foreman_uuid_boot/uuidboot_host_facet.rb
+++ app/models/foreman_uuid_boot/uuidboot_host_facet.rb
@@ -7,7 +7,8 @@ module ForemanUuidBoot
     def self.populate_fields_from_facts(host, parser, _type, _proxy)
       facet = host.uuidboot_facet || host.build_uuidboot_facet
       facet.uuid = parser.facts.dig('dmi', 'product', 'uuid') || parser.facts['uuid']
-      facet.save if facet.changed?
+      facet.save if facet.uuid && facet.changed?
+      facet.destroy if !facet.uuid && facet.persisted?
     end
   end
 end
